e96d26f2834070839a27f20fb2e891eb50ee5a30
artfrc <crfernandesarthur@gmail.com>
2022-09-10 11:53:58 -0300

    Media Files (Audio)

---
+++ files/mediaFiles/audio/app/src/main/java/com/androiddev/audioproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androiddev.audioproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
